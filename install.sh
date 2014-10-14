37955f96515a3ef24ae2bef64c911ab5ab320361
Ne02ptzero <l.solofrizzo@gmail.com>
2014-10-14 15:32:30 +0200

    Add of vimproc and Vimshell

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ git submodule init
 git submodule update
 ln bundle/ftcolor.vim/plugin/ftcolor.vim bundle/ftcolor.vim/.
 ln bundle/smartgf.vim/plugin/smartgf.vim bundle/smartgf.vim/.
+cd bundle/vimproc.vim/ ; make
